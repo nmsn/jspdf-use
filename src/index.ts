d6dec4b2754d03b0844e9824378cf5e055c807e3
ningshangyuan <ningshangyuan@hikvision.com>
2023-11-22 11:06:25 +0800

    feat: 添加 table 功能

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import JsPdf from "jspdf";
-import "jspdf-autotable";
+import autoTable, { UserOptions } from "jspdf-autotable";
 import { blob2Base64, blobToUrl, loadImage, url2Base64, isBlob } from "./utils";
 
 type AlignType = "left" | "center" | "right";
-type PaddingType = number | { left: number; top: number; right: number; bottom: number };
-type ImgType = 'url' | 'base64' | 'blob'
+type PaddingType =
+  | number
+  | { left: number; top: number; right: number; bottom: number };
+type ImgType = "url" | "base64" | "blob";
 
 class JspdfUse {
   pdf: JsPdf;
@@ -18,6 +20,8 @@ class JspdfUse {
   x: number;
   y: number;
   fontSize: number;
+  record: unknown[];
+  catalog: string;
 
   constructor(config) {
     const { pageSize = "a4", unit = "px", orientation = "portrait" } = config;
@@ -25,57 +29,65 @@ class JspdfUse {
     const pdf = new JsPdf("p", "px", "a4");
     this.pdf = pdf;
 
-    this.pageWidth  = pdf.internal.pageSize.getWidth();
+    this.pageWidth = pdf.internal.pageSize.getWidth();
     this.pageHeight = pdf.internal.pageSize.getHeight();
+
+    this.record = [];
   }
 
-  async drawImg(img: string | Blob, {
-    x = this.x,
-    y = this.y,
-    width,
-    align = "center",
-    // TODO 后续自动计算图片类型
-    type = "base64",
-  }: {
-    x: number,
-    y: number,
-    width: number,
-    align: AlignType,
-    // TODO 后续自动计算图片类型
-    type: ImgType,
-  }) {
-    const _padding = typeof this.padding === 'object' ? (this.padding.left + this.padding.right) : this.padding;
+  async drawImg(
+    img: string | Blob,
+    {
+      x = this.x,
+      y = this.y,
+      width,
+      align = "center",
+      // TODO 后续自动计算图片类型
+      type = "base64",
+    }: {
+      x: number;
+      y: number;
+      width: number;
+      align: AlignType;
+      // TODO 后续自动计算图片类型
+      type: ImgType;
+    }
+  ) {
+    const _padding =
+      typeof this.padding === "object"
+        ? this.padding.left + this.padding.right
+        : this.padding;
     const validMaxWidth = this.pageWidth - _padding;
-    
+
     // TODO 和下面流程有冗余
     const _img = await (async () => {
-      if (type === 'blob' || isBlob(img)) {
+      if (type === "blob" || isBlob(img)) {
         const url = blobToUrl(img);
-        
+
         return await loadImage(url);
       }
 
       return await loadImage(img);
     })();
 
-    const imgBase64 = await (async () => {
-      if (type === 'base64' && typeof img === 'string') {
+    const imgBase64 = (await (async () => {
+      if (type === "base64" && typeof img === "string") {
         return img;
       }
 
-      if (type === 'url' && typeof img === 'string') {
+      if (type === "url" && typeof img === "string") {
         return await url2Base64(img);
       }
 
-      if (type === 'blob' && isBlob(img)) {
+      if (type === "blob" && isBlob(img)) {
         return await blob2Base64(img);
       }
 
-      throw new Error('Image Error');
-    })() as string;
+      throw new Error("Image Error");
+    })()) as string;
 
     const { width: imgWidth, height: imgHeight } = _img;
-    
+
     let scale = 1;
     const _width = (() => {
       if (width) {
@@ -93,7 +105,7 @@ class JspdfUse {
 
     const _height = scale > 1 ? imgHeight / scale : imgHeight;
 
-    this.pdf.addImage(imgBase64, 'JPEG', x, y, _width, _height, '', 'FAST');
+    this.pdf.addImage(imgBase64, "JPEG", x, y, _width, _height, "", "FAST");
 
     const endY = y + _height;
 
@@ -102,29 +114,36 @@ class JspdfUse {
 
   drawText(
     text: string,
-    { x = this.x, y = this.y, fontSize = this.fontSize, align = "center", padding = this.padding }:{
-      x: number,
-      y: number,
-      fontSize: number,
-      align: AlignType,
-      padding: PaddingType,
+    {
+      x = this.x,
+      y = this.y,
+      fontSize = this.fontSize,
+      align = "center",
+      padding = this.padding,
+    }: {
+      x: number;
+      y: number;
+      fontSize: number;
+      align: AlignType;
+      padding: PaddingType;
     }
   ) {
-
     // TODO ts 类型上没有 pdf.internal.getFontSize 实际可执行
-    const textWidth = this.pdf.getStringUnitWidth(text) * this.pdf.getFontSize() / this.pdf.internal.scaleFactor;
+    const textWidth =
+      (this.pdf.getStringUnitWidth(text) * this.pdf.getFontSize()) /
+      this.pdf.internal.scaleFactor;
 
     const _x = (() => {
-      const paddingLeft = typeof padding === 'object' ? padding.left : padding;
-      if (align === 'center') {
+      const paddingLeft = typeof padding === "object" ? padding.left : padding;
+      if (align === "center") {
         return this.pageWidth - paddingLeft;
       }
 
-      if (align === 'left') {
-        return paddingLeft
+      if (align === "left") {
+        return paddingLeft;
       }
 
-      if (align === 'right') {
+      if (align === "right") {
         return this.pageWidth - paddingLeft - textWidth;
       }
 
@@ -141,7 +160,29 @@ class JspdfUse {
     this.y = endY;
   }
 
-  drawTitle() {}
+  addPage() {
+    this.pdf.addPage();
+  }
+
+  addRecord(text: string, pageNum: number, level: number) {
+    this.record.push({
+      text,
+      pageNum,
+      level: 1,
+    });
+  }
+
+  drawTable({ theme = "grid", ...tableConfig }: UserOptions, { y }) {
+    // FIXME autoTable v4 会修改用法
+    autoTable(this.pdf, {
+      theme,
+      ...tableConfig,
+    });
+
+    this.y = (
+      this.pdf as any as { lastAutoTable: { finalY: number } }
+    ).lastAutoTable.finalY;
+  }
 }
 
 export default JspdfUse;
